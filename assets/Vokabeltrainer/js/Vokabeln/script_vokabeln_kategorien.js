6c77376db3cfb1cae9cbcb2040e81a4c4172f9af
EstnischIntensiv <intensivestnisch@gmail.com>
2024-10-14 08:01:57 +0200

    Add files via upload

---
+++ assets/Vokabeltrainer/js/Vokabeln/script_vokabeln_kategorien.js
@@ -0,0 +1,184 @@
+// var vokabeln_zahlen = [
+//   (("1"), ("üks")),
+//   (("2"), ("kaks")),
+//   (("3"), ("kolm"))
+// ];
+
+var vokabeln_zahlen = [
+  ["eins", "üks"],
+  ["zwei", "kaks"],
+  ["drei", "kolm"],
+];
+
+var vokabeln_tiere = [
+  ["Pinguin", "pingviin"],
+  ["Hund", "koer"],
+  ["Katze", "kass"],
+];
+
+var vokabeln_essen_und_trinken = [
+  ["Wasser", "vesi"],
+  ["Kaffee", "kohv"],
+  ["Tee", "tee"],
+  ["Saft", "mahl"],
+  ["Bier", "õlu"],
+  ["Milch", "piim"],
+];
+
+var vokabeln_farben = [
+  ["rot", "punane"],
+  ["blau", "sinine"],
+  ["gelb", "kollane"],
+];
+
+
+
+
+// var vokabeln_zahlen = [
+//   (("1"), ("üks")),
+//   (("2"), ("kaks")),
+//   (("3"), ("kolm")),
+//   (("4"), ("neli")),
+//   (("5"), ("viis")),
+//   (("6"), ("kuus")),
+//   (("7"), ("seitse")),
+//   (("8"), ("kaheksa")),
+//   (("9"), ("üheksa")),
+//   (("10"), ("kümme")),
+// ];
+
+
+
+
+// var vokabeln_tiere = [
+//   ["Pinguin", "pingviin"],
+//   ["Hund", "koer"],
+//   ["Katze", "kass"],
+//   ["Fisch", "kala"],
+//   ["Bär", "karu"],
+//   ["Wolf", "hunt"],
+//   ["Huhn", "kana"],
+//   ["Schaf", "lammas"],
+//   ["Kuh", "lehm"],
+//   ["Elefant", "elevant"],
+//   ["Maus", "hiir"],
+//   ["Eichhörnchen", "orav"],
+//   ["Igel", "siil"],
+//   ["Pferd", "hobune"],
+//   ["Löwe", "lõvi"],
+//   ["Tiger", "tiiger"],
+//   ["Affe", "ahv"],
+//   ["Giraffe", "kaelkirjak"],
+//   ["Hase", "jänes"],
+//   ["Ratte", "rott"],
+//   ["Vogel", "lind"],
+//   ["Esel", "eesel"],
+//   ["Ente", "part"],
+//   ["Gans", "hani"],
+//   ["Schlange", "madu"],
+//   ["Delfin", "delfiin"],
+//   ["Tintenfisch", "kaheksajalg"],
+//   ["Biene", "mesilane"],
+//   ["Ameise", "sipelgas"]
+// ];
+
+
+
+
+// var vokabeln_essen_und_trinken = [
+//   ["Wasser", "vesi"],
+//   ["Kaffee", "kohv"],
+//   ["Tee", "tee"],
+//   ["Saft", "mahl"],
+//   ["Limonade", "limonaad"],
+//   ["Bier", "õlu"],
+//   ["Wein", "vein"],
+  
+//   ["Milch", "piim"],
+//   ["Käse", "juust"],
+  
+//   ["Brot", "leib"],
+//   ["Reis", "riis"],
+//   ["Nudeln", "nuudlid"],
+//   ["Suppe", "supp"],
+//   ["Pizza", "pitsa"],
+//   ["Hamburger", "hamburger"],
+//   ["Sandwich", "võileib"],
+//   ["Keks", "küpsis"],
+//   ["Schokolade", "šokolaad"],
+//   ["Eis", "jäätis"],
+  
+//   ["Fleisch", "liha"],
+//   ["Fisch", "kala"],
+  
+//   ["Obst", "puuvili"],
+//   ["Gemüse", "köögivili/juurvili"],
+//   ["Äpfel", "õun"],
+//   ["Banane", "banaan"],
+//   ["Beere", "mari"],
+//   ["Erdbeere", "maasikas"],
+//   ["Traube", "viinamari"],
+//   ["Karotte", "porgand"],
+//   ["Tomate", "tomat"],
+//   ["Zwiebel", "sibul"],
+//   ["Knoblauch", "küüslauk"],
+//   ["Salat", "salat"],
+//   ["Kartoffel", "kartul"],
+  
+//   ["Zucker", "suhkur"],
+//   ["Salz", "sool"],
+//   ["Pfeffer", "pipar"],
+//   ["Öl", "õli"],
+//   ["Essig", "äädikas"],
+//   ["Honig", "mesi"],
+//   ["Joghurt", "jogurt"],
+//   ["Marmelade", "moos"],
+  
+//   ["Nuss", "pähkel"],
+//   ["Popcorn", "popkorn"],
+//   ["Pfannkuchen", "pannkook"],
+//   ["Wurst", "vorst"],
+//   ["Sahne", "koor"],
+//   ["Pudding", "puding"],
+//   ["Torte", "kook"]
+// ];
+
+
+
+// var vokabeln_farben = [
+//   ["rot", "punane"],
+//   ["blau", "sinine"],
+//   ["gelb", "kollane"],
+//   ["grün", "roheline"],
+//   ["schwarz", "must"],
+//   ["weiß", "valge"],
+//   ["orange", "oranž"],
+//   ["lila", "lilla"],
+//   ["braun", "pruun"],
+//   ["grau", "hall"],
+//   ["rosa", "roosa"],
+//   ["hell", "hele"],
+//   ["dunkel", "tume"],
+//   ["hellblau", "helesinine"],
+//   ["dunkelblau", "tumesinine"]
+// ];
+
+
+
+
+
+// var vokabelanzahl = [
+//   ["Zahlen", vokabeln_zahlen.length],
+//   ["Tiere", vokabeln_tiere.length],
+//   ["Essen & Trinken", vokabeln_essen_und_trinken.length],
+//   ["Farben", vokabeln_farben.length],
+//   ["Kleidung", 18],
+//   ["Möbel & Haushalt", 22],
+//   ["Familie & Freunde", 19],
+//   ["Bildung", 24],
+//   ["Freizeit", 20],
+//   ["Wetter", 17],
+//   ["Reisen & Transport", 28],
+//   ["Politik", 10],
+//   ["Zeit & Datum", 14]
+// ];
